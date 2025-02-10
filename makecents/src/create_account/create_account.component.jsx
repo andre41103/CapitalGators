0ae0ae557255378bb56498aef771747dec3e3449
andreasanchez <andre41103@gmail.com>
2025-02-09 20:17:32 -0500

    Added labels with input boxes

--- makecents/src/create_account/create_account.component.jsx
+++ makecents/src/create_account/create_account.component.jsx
@@ -12,11 +12,41 @@ const Create_Account = () => {
     };
 
     return (
-        <div className="contentHeader">
+        <div className="content">
           <h1 className='custom-h1'>Welcome to MakeCents!</h1>
           <h2 className='custom-h2'>Below we will ask you questions that will personalize your experience with us</h2>
-          <button onClick={handleLogin}>Return to Login</button>
+          
+          <div className="form-container">
+            <div className="form-group">
+              <label htmlFor='name'>Name:</label>
+              <input type="text" id="name" name="name" placeholder="Full name" />
+            </div>
+          </div>
+
+          <div className="form-container">
+            <div className="form-group">
+              <label htmlFor='email address'>Email Address:</label>
+              <input type="text" id="email address" name="email address" placeholder="Valid Email Address" />
+            </div>
+          </div>
+
+          <div className="form-container">
+            <div className="form-group">
+              <label htmlFor='monthly income'>What is your monthly income? :</label>
+              <input type="text" id="monthly income" name="monthly income" placeholder="$" />
+            </div>
+          </div>
+
+          <div className="form-container">
+            <div className="form-group">
+              <label htmlFor='monthly spending goal'>What is your ideal monthly spending goal? :</label>
+              <input type="text" id="monthly spending goal" name="monthly spending goal" placeholder="$" />
+            </div>
+          </div>
+
+         
         </div>
+
     );
     
   }
