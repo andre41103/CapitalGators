e11699f880bf568d8d9f93d45633ab54b9ec9788
andreasanchez <andre41103@gmail.com>
2025-02-19 11:18:13 -0800

    Added profile button

--- makecents/src/dashboard/dashboard.component.jsx
+++ makecents/src/dashboard/dashboard.component.jsx
@@ -3,10 +3,18 @@ import './dashboard.component.css';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const handleProfile = () => {
+    navigate('/profile');
+  };
     return (
       <div className="dashboard">
         <h2>Welcome to the Dashboard!</h2>
+        <button onClick={handleProfile} className="button">Profile</button>
       </div>
+
+      
     );
   }
   
