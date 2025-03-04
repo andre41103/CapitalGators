518bed8fb884edbb6e681a19c494016ef526e6aa
andreasanchez <andre41103@gmail.com>
2025-03-04 15:51:51 -0500

    Resource Page structure

--- makecents/src/resources/resources.component.jsx
+++ makecents/src/resources/resources.component.jsx
@@ -4,9 +4,25 @@ import { useNavigate } from 'react-router-dom';
 
 const Resources = () => {
     return (
-      <div className="resources">
-        <h2>Welcome to the Resources Page!</h2>
+      <div className="resources-content">
+        <div className="top-left-title">
+          <h2>MakeCents</h2>
+        </div>
+        <div className="vertical-containers">
+          <div className='resources-container-wrapper'>
+            <h3 className='container-title'>Online Resources</h3>
+          <div className="individual-vertical-container"></div>
+          </div>
+
+        <div className='resources-container-wrapper'>
+        <h3 className='container-title'>Financial Chatbox</h3>
+          <div className="individual-vertical-container"></div>
+        </div>
+
+        <footer className='resources-footer'></footer>
       </div>
+    </div>
+      
     );
   }
   
