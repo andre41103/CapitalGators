115e31faf7a24bb424a2e12525029cdf82782a56
andreasanchez <andre41103@gmail.com>
2025-03-25 21:59:04 -0400

    Fixed character issues in credit card info

--- makecents/src/resources/resources.component.jsx
+++ makecents/src/resources/resources.component.jsx
@@ -22,9 +22,10 @@ const Resources = () => {
               
               // some cards in the json have the trademark sign as the code number so we need to change it to the actual symbol
               const modifiedData = data.props.pageProps.pageData['category-page'].collection.cards.map(card => {
-                card.name = card.name.replace(/&#174;/g, '®');
+                card.name = card.name.replace(/&#174;|&reg;/g, '®');
                 return card;
               });
+
           
               setCards(modifiedData);  // Set the modified data with the trademark symbol
           } catch (error) {
